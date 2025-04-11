60d73d15226cc300178f9a8de43f7a6f566e6397
Mohit Duhan <mohitkumar72956@gmail.com>
2025-04-11 12:22:12 +0530

    bst

---
+++ Lec24_Lab/convert_sorted_array_bst.java
@@ -0,0 +1,38 @@
+package U.Lec24_Lab;
+
+public class convert_sorted_array_bst {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode construct(int[] nums, int left, int right) {
+        if (left > right) return null;
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+
+        root.left = construct(nums, left, mid - 1);
+        root.right = construct(nums, mid + 1, right);
+
+        return root;
+    }
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        TreeNode root = construct(nums, 0, nums.length - 1);
+        return root;
+    }
+}
---
+++ Lec24_Lab/kth_smallest_element.java
@@ -0,0 +1,34 @@
+package U.Lec24_Lab;
+
+import U.Lec23_Theory.Delete_Node_bst;
+
+public class kth_smallest_element {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    int count = 0 , temp = 0;
+    public void inorder(TreeNode root , int k){
+        if(root == null)return;
+
+        inorder(root.left , k);
+        count++;
+        if(count == k){
+            temp = root.val;
+        }
+        inorder(root.right , k);
+    }
+    public int kthSmallest(TreeNode root, int k) {
+        if(root == null)return 0;
+        inorder(root ,k);
+        return temp;
+    }
+}
